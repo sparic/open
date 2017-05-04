02d7bb1ae44f284369763d3e74cf9de1f8a56657
ray.fu <ray.fu@mrobot.cn>
2017-05-04 14:56:32 +0800

    1.配置文件数据库的编码方式改成UTF-8 2.项目编码导致的配置文件中文乱码修复

--- src/main/java/cn/muye/controller/VersionController.java
+++ src/main/java/cn/muye/controller/VersionController.java
@@ -8,7 +8,8 @@ import cn.muye.service.VersionService;
 import com.github.pagehelper.PageHelper;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +23,7 @@ import java.util.List;
 @RequestMapping("/version")
 public class VersionController {
 
-    private static final Logger LOGGER = Logger.getLogger(VersionController.class);
+    private static Logger LOGGER = LoggerFactory.getLogger(VersionController.class);
 
     @Autowired
     private VersionService versionService;
@@ -39,8 +40,8 @@ public class VersionController {
      */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ApiOperation(value = "查询版本列表", httpMethod = "GET", notes = "查询版本列表")
-    public AjaxResult getVersionList(@ApiParam(value = "页号")@RequestParam(value = "page", required = false) Integer page,
-                                     @ApiParam(value = "每页记录数")@RequestParam(value = "pageSize", required = false) Integer pageSize) {
+    public AjaxResult getVersionList(@ApiParam(value = "页号") @RequestParam(value = "page", required = false) Integer page,
+                                     @ApiParam(value = "每页记录数") @RequestParam(value = "pageSize", required = false) Integer pageSize) {
         if (page != null && pageSize != null) {
             PageHelper.startPage(page, pageSize);
         }
@@ -56,7 +57,7 @@ public class VersionController {
      */
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @ApiOperation(value = "新增版本", httpMethod = "POST", notes = "新增版本列表")
-    public AjaxResult postVersion(@ApiParam(value = "版本对象")@RequestBody Version version) {
+    public AjaxResult postVersion(@ApiParam(value = "版本对象") @RequestBody Version version) {
         return post(version);
     }
 
@@ -74,7 +75,7 @@ public class VersionController {
      */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ApiOperation(value = "获取单个版本", httpMethod = "GET", notes = "获取单个版本")
-    public AjaxResult getVersion(@ApiParam(value = "版本ID")@PathVariable Long id) {
+    public AjaxResult getVersion(@ApiParam(value = "版本ID") @PathVariable Long id) {
         Version version = versionService.getById(id);
         return AjaxResult.success(version);
     }
@@ -88,7 +89,7 @@ public class VersionController {
      */
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @ApiOperation(value = "修改版本", httpMethod = "PUT", notes = "修改版本")
-    public AjaxResult putVersion(@ApiParam(value = "版本ID")@PathVariable Long id, @ApiParam(value = "版本对象")@RequestBody Version version) {
+    public AjaxResult putVersion(@ApiParam(value = "版本ID") @PathVariable Long id, @ApiParam(value = "版本对象") @RequestBody Version version) {
         Version versionDb = versionService.getById(id);
         if (versionDb != null) {
             versionDb.setVersionCode(version.getVersionCode());
@@ -110,7 +111,7 @@ public class VersionController {
      */
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ApiOperation(value = "删除版本", httpMethod = "DELETE", notes = "删除版本")
-    public AjaxResult deleteVersion(@ApiParam(value = "版本ID")@PathVariable Long id) {
+    public AjaxResult deleteVersion(@ApiParam(value = "版本ID") @PathVariable Long id) {
         versionService.deleteById(id);
         return AjaxResult.success();
     }
@@ -123,9 +124,9 @@ public class VersionController {
      */
     @RequestMapping(value = "/copy", method = RequestMethod.POST)
     @ApiOperation(value = "拷贝版本", httpMethod = "POST", notes = "拷贝版本")
-    public AjaxResult copyVersion(@ApiParam(value = "版本ID")@RequestParam(value = "versionId", required = true) Long versionId,
-                                  @ApiParam(value = "版本编号")@RequestParam(value = "versionCode", required = true) String versionCode,
-                                  @ApiParam(value = "版本描述")@RequestParam(value = "description", required = false) String description) {
+    public AjaxResult copyVersion(@ApiParam(value = "版本ID") @RequestParam(value = "versionId", required = true) Long versionId,
+                                  @ApiParam(value = "版本编号") @RequestParam(value = "versionCode", required = true) String versionCode,
+                                  @ApiParam(value = "版本描述") @RequestParam(value = "description", required = false) String description) {
         Version versionNew = new Version();
         versionNew.setDescription(description);
         versionNew.setVersionCode(versionCode);
